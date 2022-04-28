12069b67bb9bc1f14a1f4f348e4d64b15162bbc6
FursDominik <fdsfurs007@gmail.com>
2022-04-28 23:21:27 +0300

    Добавьте файлы проекта.

---
+++ rekursia/rekursia.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+
+using namespace std;
+
+double recursive(int m, int k)
+{
+    double res = 0;
+    if (k == m) {
+        res = sqrt(m);
+    }
+    if (k < m) {
+        res = sqrt(k + recursive(m, k + 1));
+    }
+    return res;
+}
+void non_recursive(int m)
+{
+    double res = 0;
+    for (int i = m; i > 0; i--)
+    {
+        res = sqrt(i + res);
+    }
+    cout << res << endl;
+}
+
+int main()
+{
+    int n, k;
+    double res;
+    cout << "Enter n" << endl;
+    cin >> n;
+    cout << "Recursive way:" << endl;
+    res = recursive(n, 1);
+    cout << res << endl;
+    cout << "Non-recursive way:" << endl;
+    non_recursive(n);
+    system("PAUSE");
+    return 0;
+}
